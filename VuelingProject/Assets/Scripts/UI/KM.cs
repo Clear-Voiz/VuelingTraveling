73363eebe53f87b1caf99db835eddff50d1cbb1f
Jordi Aragon <joaragonca@gmail.com>
2023-05-13 11:43:36 +0200

    Trying WebRequest

--- VuelingProject/Assets/Scripts/UI/KM.cs
+++ VuelingProject/Assets/Scripts/UI/KM.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace UI
 {
@@ -7,14 +9,49 @@ namespace UI
     {
         public float km;
         public TextMeshProUGUI displayer;
-        
+
+
+        private void OnEnable()
+        {
+            PlaneCollision.OnGameOver += SendSignal;
+        }
+        private void OnDisable()
+        {
+            PlaneCollision.OnGameOver -= SendSignal;
+        }
 
         private void Update()
         {
             if (GameManager.Instance.playerStats == null) return;
             km += Time.deltaTime * GameManager.Instance.playerStats.speed;
             displayer.text = "Km: " + (int)km;
+            
+        }
 
+        private void SendSignal()
+        {
+            StartCoroutine(SendWebRequest());
+            
+        }
+
+        private IEnumerator SendWebRequest()
+        {
+             WWWForm form = new WWWForm();
+             form.AddField("extracoins","5");
+             form.AddField("punctuation", ""+(int)km);
+             UnityWebRequest www = UnityWebRequest.Post("https://hack2023-vueling.onrender.com/api/v1/game/645ece91092a2fab071cf86d", form);
+             yield return www.SendWebRequest();
+
+             if (www.result != UnityWebRequest.Result.Success)
+             {
+                 Debug.Log(www.error);
+             }
+             else
+             {
+                 Debug.Log("Form upload complete!");
+                 km = 0f;
+             }
+             //yield return www.SendWebRequest();
         }
     }
 }
